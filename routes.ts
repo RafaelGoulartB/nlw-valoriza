f90c68c8de91b05f670ba2fac436583a9476088a
RafaelGoulartB <rafagoulartb@gmail.com>
2021-06-23 17:00:33 -0300

    📦 feat: create middleware to ensure that user is admin

--- routes.ts
+++ routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { CreateTagController } from './src/controllers/CreateTagController'
 import { CreateUserControllers } from './src/controllers/CreateUserControllers'
+import { ensureAdmin } from './src/middleware/ensureAdmin'
 
 const router = Router()
 
@@ -9,6 +10,6 @@ const createTagController = new CreateTagController()
 
 router.post('/users', createUserController.handle)
 
-router.post('/tags', createTagController.handle)
+router.post('/tags', ensureAdmin, createTagController.handle)
 
 export { router }
