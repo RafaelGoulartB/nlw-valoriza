7a29676f7352431ed101ee935a2bb43c1dade53c
RafaelGoulartB <rafagoulartb@gmail.com>
2021-06-23 16:24:20 -0300

    📦 feat: add async error handle as middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import { router } from '../routes'
 
 import './database'
@@ -8,5 +9,18 @@ const app = express()
 
 app.use(express.json())
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: 'Internal Server Error'
+  })
+})
 
 app.listen(3333, () => console.log(`Server is running at http://localhost:3333`))
